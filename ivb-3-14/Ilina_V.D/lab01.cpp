78b12a2cc0814744e2b5dd50bc1ca302608bc5b2
catwomenko <veronikailina96@gmail.com>
2014-12-24 20:10:57 +0300

    fixed the first lab source

--- ivb-3-14/Ilina_V.D/lab01.cpp
+++ ivb-3-14/Ilina_V.D/lab01.cpp
@@ -1,250 +1,61 @@
-/* 10 Даны две матрицы разного размера. Для той из матриц, в которой меньше
-количество нулевых элементов, найти минимальный элемент в каждой строке. */
+/*
+
+10
+Определить в каком массиве меньше среднее арифметическое элементов, меньших 
+заданного числа. Если в двух или трех массивах значения среднего 
+арифметического совпадают, вывести соответствующее сообщение.
+
+*/
 
 #pragma warning(disable: 4996)
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
-static double** __loadMatrix(
-	const char* const szFileName,
-	int* piRows,
-	int* piCols);
+#define MATRIX1 4
+#define MATRIX2 2
+#define MATRIX3 6
 
-static void __destroyMatrix(double** pMatrix, int rows, int cols);
+static double m1[] = { 1., 1., 1., 1. };
+static double m2[] = { 1., 1. };
+static double m3[] = { 4., 4., 4., 4., 4., 4. };
 
-static int __exception(const char* const szMessage)
+static double
+calculate(double matrix[], int elements, const double part)
 {
-	fprintf(stderr, "%s\n", szMessage);
-	return EXIT_FAILURE;
+	double result = 0;
+	int i;
+	int n = 0;
+
+	for (i = 0; i < elements; ++i) {
+		if (matrix[i] < part) {
+			result += matrix[i];
+			++n;
+		}
+	}
+	return result == 0 || n == 0 ? 0 : result / n;
 }
 
-static void __printMatrix(double** pMatrix, int rows, int cols);
-static double __findMaxElement(double** pMatrix, int rows, int cols);
-static void __outputMaxElements(double** pMatrix, int rows, int cols);
-
-int main(int argc, char** argv)
+int
+main(int argc, char **argv)
 {
-	if (argc < 3) {
-		return __exception("Not found input file");
-	}
+	const double part = 4.;
+	double mr1 = calculate(m1, MATRIX1, part);
+	double mr2 = calculate(m2, MATRIX2, part);
+	double mr3 = calculate(m3, sizeof(m3) / sizeof(m3[0]), part);
 
-	int mRows1 = 0;
-	int mCols1 = 0;
-	int mRows2 = 0;
-	int mCols2 = 0;
-	double** matrix1 = __loadMatrix(argv[1], &mRows1, &mCols1);
-	double** matrix2 = __loadMatrix(argv[2], &mRows2, &mCols2);
-
-	fprintf(stdout, "Matrix N1:\n");
-	__printMatrix(matrix1, mRows1, mCols1);
-	fprintf(stdout, "Matrix N2:\n");
-	__printMatrix(matrix2, mRows2, mCols2);
-
-	double max1 = __findMaxElement(matrix1, mRows1, mCols1);
-	double max2 = __findMaxElement(matrix2, mRows2, mCols2);
-
-	if (max1 > max2) {
-		fprintf(stdout, "Output matrix N1:\n");
-		__outputMaxElements(matrix1, mRows1, mCols1);
-	}
-	else {
-		fprintf(stdout, "Output matrix N2:\n");
-		__outputMaxElements(matrix2, mRows2, mCols2);
-	}
-	__destroyMatrix(matrix1, mRows1, mCols1);
-	__destroyMatrix(matrix2, mRows2, mCols2);
+	if (mr1 < mr2 && mr1 < mr3)
+		fprintf(stdout, "Среднее арифметического для первого массива наименьшее.\n");
+	if (mr2 < mr1 && mr2 < mr3)
+		fprintf(stdout, "Среднее арифметического для второго массива наименьшее.\n");
+	if (mr3 < mr1 && mr3 < mr2)
+		fprintf(stdout, "Среднее арифметического для третьего массива наименьшее.\n");
+	if (mr1 == mr2)
+		fprintf(stdout, "Среднее арифметическое первого и второго массивов равны.\n");
+	if (mr1 == mr3)
+		fprintf(stdout, "Среднее арифметическое первого и третьего массивов равны.\n");
+	if (mr2 == mr3)
+		fprintf(stdout, "Среднее арифметическое второго и третьего массивов равны.\n");
 	system("pause");
 	return EXIT_SUCCESS;
-}
-
-#include <string>
-#include <vector>
-
-class NumberFromFileParser
-{
-	typedef std::vector<double> __MatrixLine;
-	typedef std::vector<__MatrixLine > __Matrix;
-
-	friend double**
-		__loadMatrix(
-		const char* const szFileName,
-		int* piRows,
-		int* piCols);
-public:
-	NumberFromFileParser() :_ch(0), _fd(nullptr) {}
-	~NumberFromFileParser() {}
-
-	bool eof() const
-	{
-		return feof(_fd) > 0;
-	}
-
-	bool next()
-	{
-		_ch = fgetc(_fd);
-		return _ch != EOF;
-	}
-
-	bool isWhiteSpace() const
-	{
-		return isspace((int)_ch) > 0;
-	}
-
-	bool isDigit() const
-	{
-		return isdigit((int)_ch) > 0;
-	}
-
-	bool isDot() const
-	{
-		return _ch == '.' || _ch == ',';
-	}
-
-	bool isSign() const
-	{
-		return _ch == '+' || _ch == '-';
-	}
-
-	bool isEndOfLine() const
-	{
-		return _ch == '\r' || _ch == '\n';
-	}
-
-	void skipWhiteSpace()
-	{
-		while (!eof() && isWhiteSpace() && next());
-	}
-
-	void skipEndOfLine()
-	{
-		while (!eof() && isEndOfLine() && next());
-	}
-
-	void parseNumber() {
-		skipWhiteSpace();
-		while (!eof() && !isWhiteSpace() &&
-			(isSign() || isDot() || isDigit())) {
-			put();
-			next();
-		}
-	}
-
-	void put()
-	{
-		_buffer.push_back(_ch);
-	}
-
-	bool parse(const char* const name, __Matrix &matrix)
-	{
-		std::vector<double> row;
-		matrix.clear();
-		if (_fd == nullptr)
-			_fd = fopen(name, "r");
-		if (_fd == nullptr)
-			return false;
-		next();
-		while (!eof()) {
-			_buffer.clear();
-			parseNumber();
-			if (_buffer.size() > 0) {
-				row.push_back(atof(_buffer.c_str()));
-			}
-			if (isEndOfLine()) {
-				skipEndOfLine();
-				matrix.push_back(row);
-				row.clear();
-			}
-		}
-		if (row.size() > 0)
-			matrix.push_back(row);
-		fclose(_fd);
-		_fd = nullptr;
-		return true;
-	}
-private:
-	char _ch;
-	FILE *_fd;
-	std::string _buffer;
-};
-
-double** __loadMatrix(
-	const char* const szFileName,
-	int* piRows,
-	int* piCols)
-{
-	NumberFromFileParser parser;
-	NumberFromFileParser::__Matrix matrix;
-	auto retval = parser.parse(szFileName, matrix);
-	if (retval) {
-		(*piRows) = matrix.size();
-		double** result = new double*[matrix.size()];
-		for (
-			NumberFromFileParser::__MatrixLine::size_type k = 0;
-			k < matrix.size();
-			k++) {
-			NumberFromFileParser::__MatrixLine line = matrix.at(k);
-			result[k] = new double[line.size()];
-			(*piCols) = line.size();
-			for (
-				NumberFromFileParser::__Matrix::size_type i = 0;
-				i < line.size();
-			i++) {
-				result[k][i] = line.at(i);
-			}
-		}
-			return result;
-	}
-	return nullptr;
-}
-
-void
-__destroyMatrix(double **pMatrix, int rows, int cols)
-{
-	if (pMatrix != nullptr) {
-		for (auto i = 0; i < rows; i++) {
-			if (pMatrix[i] != nullptr)
-				delete[] pMatrix[i];
-		}
-		delete[] pMatrix;
-	}
-}
-
-void
-__printMatrix(double **pMatrix, int rows, int cols)
-{
-	for (auto i = 0; i < rows; ++i) {
-		for (auto j = 0; j < cols; ++j) {
-			if (j > 0)
-				fprintf(stdout, " ");
-			fprintf(stdout, "%3.5f", pMatrix[i][j]);
-		}
-		fprintf(stdout, "\n");
-	}
-}
-
-double
-__findMaxElement(double **pMatrix, int rows, int cols)
-{
-	double result = pMatrix[0][0];
-	for (auto i = 0; i < rows; ++i) {
-		for (auto j = 0; j < cols; ++j) {
-			if (result <= pMatrix[i][j])
-				result = pMatrix[i][j];
-		}
-	}
-	return result;
-}
-
-void
-__outputMaxElements(double **pMatrix, int rows, int cols)
-{
-	for (auto i = 0; i < rows; ++i) {
-		double max = pMatrix[i][0];
-		for (auto j = 0; j < cols; ++j) {
-			if (max <= pMatrix[i][j])
-				max = pMatrix[i][j];
-		}
-		fprintf(stdout, "%3.5f\n", max);
-	}
 }
